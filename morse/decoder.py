4c4fd94a1dc923d1f6f9674a3b63499b8b9afb5f
Macbook Air <macbookair@Ka-Chow-Air.local>
2026-02-10 19:47:10 +0300

    Finished the decoder

--- morse/decoder.py
+++ morse/decoder.py
@@ -1,9 +1,35 @@
-pass  # YOUR CODE HERE
+from morse.mapping import MORSE
+
+REVERSE_MORSE = {value: key for key, value in MORSE.items()}
+
+
+def decode_word(morse_word):
+    letters = []
+
+    for symbol in morse_word.split():
+        if symbol in REVERSE_MORSE:
+            letters.append(REVERSE_MORSE[symbol])
+
+    return "".join(letters)
+
+
+def decode(morse_text):
+    words = morse_text.split("|")
+    decoded_words = []
+
+    for word in words:
+        decoded_words.append(decode_word(word))
+
+    return " ".join(decoded_words)
 
 
 if __name__ == "__main__":
-    # Example usage for one word
-    pass  # YOUR CODE HERE
+    from morse.encoder import encode
 
-    # Example usage for one sentence
-    pass  # YOUR CODE HERE
+    original_text = "abc ABC"
+    morse = encode(original_text)
+    decoded = decode(morse)
+
+    print("Original: ", original_text.upper())
+    print("Morse Code: ", morse)
+    print("Decoded: ", decoded)
